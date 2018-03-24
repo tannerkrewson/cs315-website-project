8038f9148dd914d79d836b5e834efcb99267e01d
tannerkrewson <tannerkrewson@gmail.com>
2018-03-23 19:26:05 -0500

    Converted subpages to php

---
+++ project/dogs/tips.php
@@ -0,0 +1,35 @@
+<?php
+	$currentDir = "dogs";
+	include '../includes/header.php';
+?>
+<div class="article">
+	<h2>5 Dog Care Tips</h2>
+	<p>
+		<ol>
+			<li>
+				Protect your dog from common household hazards.
+				<img src="https://images.agoramedia.com/EHBlogImages/everyday-pets/2014/11/PetHolidayHazards.jpg" alt="Dog" />
+			</li>
+			<li>
+				Buy toys for your dog so they don't chew on your shoes.
+				<img src="https://vetstreet-brightspot.s3.amazonaws.com/76/de/a679f2fc48f484938863a813e38c/dog-chewing-shoes-istock-000049914060-medium-335.jpg" alt="Dog" />
+			</li>
+			<li>
+				Allow you dog to socialize early in its life.
+				<img src="https://i.ytimg.com/vi/sg1m2RJwyEU/hqdefault.jpg" alt="Dog" />
+			</li>
+			<li>
+				Don't hit your dog; gently scold it verbly instead.
+				<img src="https://gofatherhood.com/wp-content/uploads/2012/08/bad-dog-baring-teeth-300x300.png" alt="Dog" />
+			</li>
+			<li>
+				If it is your first dog, choose a more submissive breed.
+				<img src="https://i.imgur.com/yZ9IHsk.jpg" alt="Dog" />
+			</li>
+		</ol>
+	</p>
+</div>
+
+<?php
+	include '../includes/footer.php';
+?>
