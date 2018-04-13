2b18eeaa1d124b2cc0024459eb3085046f8cc074
tannerkrewson <tannerkrewson@gmail.com>
2018-04-12 21:29:51 -0500

    Added page to view contents of database

---
+++ hw5/view.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Tanner Krewson - CS 315 Homework 5</title>
+	</head>
+	<body>
+		<h2>Tanner Krewson - CS 315 Homework 5</h2>
+
+		<table border="1">
+			<tr>
+				<th>Key</th>
+				<th>First Name</th>
+				<th>Last Name</th>
+				<th>Age</th>
+			</tr>
+
+			<?php
+			$servername = "mysql.truman.edu";
+			$username = "tmk5443";
+			$password = "raewahto";
+			$dbname = "tmk5443CS315";
+
+			try {
+				$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+				// set the PDO error mode to exception
+				$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+				$result = $conn->prepare("SELECT key, firstname, lastname, age FROM hw5");
+				$result->execute();
+
+				while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+					$key = $row['key'];
+					$first_name = $row['firstname'];
+					$last_name = $row['lastname'];
+					$age = $row['age'];
+					echo "<tr>";
+					echo "<td>'$key'</td>";
+					echo "<td>'$first_name'</td>";
+					echo "<td>'$last_name'</td>";
+					echo "<td>'$age'</td>";
+					echo "</tr>";
+				}
+			} catch(PDOException $e) {
+				echo "Error: " . $e->getMessage();
+			}
+			$conn = null;
+			?>
+			</table>
+	</body>
+</html>
